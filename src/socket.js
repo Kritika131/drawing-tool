c5e1f7ac26d9705a08844ef61f6450e2d77254c4
Kritika131 <guptamushkan817@gmail.com>
2023-12-09 21:31:20 +0530

    provide the end point

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import {io} from "socket.io-client"
-
-export const socket = io("http://localhost:5000")
+const url = process.env.NODE_ENV === 'production' ? 'https://drawing-tool-server-c1iv.onrender.com' :'http://localhost:5000'
+export const socket = io(url)
